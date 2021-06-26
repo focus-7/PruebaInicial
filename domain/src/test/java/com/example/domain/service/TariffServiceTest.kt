a772e8b20ee91da7cda5457f062fec1abbeb40c3
brayan.pinilla <brayan.pinilla@ceiba.com.co>
2021-06-26 18:45:20 -0500

    Test changed

--- domain/src/test/java/com/example/domain/service/TariffServiceTest.kt
+++ domain/src/test/java/com/example/domain/service/TariffServiceTest.kt
@@ -1,7 +1,10 @@
 package com.example.domain.service
 
+import com.example.domain.aggregate.Tariff
+import com.example.domain.entity.Car
 import com.example.domain.entity.Motorcycle
 import com.example.domain.repository.TariffRepository
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
@@ -15,23 +18,47 @@ class TariffServiceTest {
     lateinit var tariffRepository: TariffRepository
 
     @get:Rule
-     val mockitoRule: MockitoRule = MockitoJUnit.rule()
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
 
     @InjectMocks
     lateinit var tariffService: TariffService
 
     @Test
-    fun testReadyForFlight() {
+    fun enterVehicle_WithCorrectParameters_successful() {
+        //Arrange
         val entryVehicle = 1624352400000 //June 22, 2021, 9:00 a.m
         val vehicle = Motorcycle("TYU78E", 150)
 
-    //    val tariffService = TariffService(tariffRepository)
-     //   val planeMock = PowerMockito.mock(TariffService::class.java)
-
-       // Mockito.`when`(planeMock.enterVehicle(entryVehicle, vehicle)).thenReturn(true)
-
+        //Assert
         assertTrue(tariffService.enterVehicle(entryVehicle, vehicle))
-     //   assertTrue(planeMock.enterVehicle(entryVehicle, vehicle))
     }
 
+    @Test
+    fun takeOut_vehicleWithCorrectParameters_successful() {
+        //Arrange
+        val entryVehicle = 1624266000000 //June 21, 2021, 9:00 a.m
+        val vehicle = Car("TYU78E")
+        val motorcycle = Tariff(entryVehicle, vehicle)
+
+        //Act
+        motorcycle.vehicleDepartureDate = 1624366800000 //June 22, 2021, 1:00 p.m
+
+        //Assert
+        assertTrue(tariffService.takeOutVehicle(motorcycle))
+    }
+
+    @Test
+    fun get_vehiclesByPlate_successful() {
+        //Arrange
+        val vehicle = Car("TYU78E")
+
+        //Act
+        assertNotNull(tariffService.getVehiclesByPlate(vehicle.plate))
+    }
+
+    @Test
+    fun get_allVehicles_successful() {
+        //Act
+        assertNotNull(tariffService.getVehicles())
+    }
 }

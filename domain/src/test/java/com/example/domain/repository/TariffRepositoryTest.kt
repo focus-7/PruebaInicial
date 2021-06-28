7267f297869eb7941e13f8bc82b9587fb5c0055e
brayan.pinilla <brayan.pinilla@ceiba.com.co>
2021-06-28 07:49:30 -0500

    Test added

--- domain/src/test/java/com/example/domain/repository/TariffRepositoryTest.kt
+++ domain/src/test/java/com/example/domain/repository/TariffRepositoryTest.kt
@@ -37,10 +37,22 @@ class TariffRepositoryTest {
     }
 
     @Test
-    fun takeOut_vehicleWithCorrectParameters_successful() {
+    fun takeOutCar_WithCorrectParameters_successful() {
         //Arrange
         val entryVehicle = 1624266000000 //June 21, 2021, 9:00 a.m
         val vehicle = Car("TYU78E")
+        val car = Tariff(entryVehicle, vehicle)
+        car.vehicleDepartureDate = 1624302000000 //June 22, 2021, 7:00 p.m
+
+        //Act
+        tariffRepository.takeOutVehicle(car)
+    }
+
+    @Test
+    fun takeOutMotorcycle_WithCorrectParameters_successful() {
+        //Arrange
+        val entryVehicle = 1624266000000 //June 21, 2021, 9:00 a.m
+        val vehicle = Motorcycle("TYU78E", 150)
         val motorcycle = Tariff(entryVehicle, vehicle)
         motorcycle.vehicleDepartureDate = 1624302000000 //June 22, 2021, 7:00 p.m
 
